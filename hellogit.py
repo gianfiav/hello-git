3ff70a82fe8825d20cd5d82c1a61db53ed6d3f7f
gianfiav <ggalli@americavirtualsa.com>
2024-05-07 11:55:41 -0300

    asd

--- hellogit.py
+++ hellogit.py
@@ -1,6 +1,4 @@
 print ("hola")
 print ("commit numero 2")
 print ("commit numero 3 junto a nuevo archivo hellogit2")
-
-print("Aca estamos en la rama MAIN")
-print("ESTO ES NUEVO, DEBERIA APARECER EN RAMA LOGIN")
+print("ACA ESTAMOS EN LA RAMA MAIN!!")
